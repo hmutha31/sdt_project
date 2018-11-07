bc6915c51af0f371229ec8fe4065cb48e63072e9
harshm31 <harshmutha31@gmail.com>
2018-11-07 21:21:39 +0530

    transfer money api done

--- expense_tracker/src/routes.php
+++ expense_tracker/src/routes.php
@@ -16,6 +16,7 @@ use Slim\Http\Response;
 $app->post('/signUp',function(Request $request , Response $response)
 {
 	require_once("db.php");
+	$def_bal = 5000;
 	$input = $request->getParsedBody();
 
 	$reg_no = $input["reg_no"];
@@ -29,6 +30,10 @@ $app->post('/signUp',function(Request $request , Response $response)
 
 	if($query->execute())
 	{
+		$query2 = $conn->prepare("INSERT INTO balance (reg_no , balance_amt) VALUES(?,?)");
+		$query2->bind_param("si",$reg_no,$def_bal);
+		$query2->execute();
+
 		$message = "Successfully Registered";
 		$code = 1;
 		$data = array("code"=>$code,"message"=>$message);
@@ -60,6 +65,20 @@ $app->get('/userDetails/{reg_no}',function(Request $request , Response $response
 	$result = $conn->query($query);
 
 	$row = $result->fetch_assoc();
+	if(sizeof($row)==0)
+	{
+		$message = "no such user found ";
+		$code = 2;
+		$data = array("code"=>$code,"message"=>$message);
+
+		return $response->withJson($data)
+						->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+	}
+
+	//else if user exists
 	$name = $row["name"];
 	$email = $row["email"];
 	$reg_no = $row["reg"];
@@ -72,11 +91,88 @@ $app->get('/userDetails/{reg_no}',function(Request $request , Response $response
 		$data[] = $row1;
 	}
 
+	if(sizeof($data)==0)
+	{
+		$data = "no monthly budget set yet ";
+	}
+
 	$message = "user details with budget ";
 	$code = 1;
 	$output = array("code" => $code , "message"=>$message,"reg"=>$reg_no,"name"=>$name,"email"=>$email,"contact"=>$contact,"data"=>$data);
 
 	return $response->withJson($output)
+						->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+});
+
+$app->post('/transferMoney',function(Request $request , Response $response , array $args)
+{
+	require_once("db.php");
+
+	$input = $request->getParsedBody();
+	$reg_no = $input["reg_no"];
+	$reg_no2 = $input["receiver_reg_no"];
+	$amount = $input["amount"];
+	$desc = $input["description"];
+
+	//checking with users balance amount
+	$query = "select balance_amt as balance from balance where reg_no = '".$reg_no."';";
+	echo($query);
+	$result = $conn->query($query);
+	$row = $result->fetch_assoc();
+	$balance = $row["balance"];
+
+	$queryx = "select balance_amt as balance from balance where reg_no = '".$reg_no2."';";
+	$resultx = $conn->query($queryx);
+	$rowx = $resultx->fetch_assoc();
+	$balancex = $rowx["balance"];
+
+	if($amount<=$balance)
+	{
+		$query2 = "select reg_no from users where reg_no = '".$reg_no2."';";
+		$result2 = $conn->query($query2);
+		$row = $result2->fetch_assoc();
+
+		//receiver exists or not ?
+		if(sizeof($row)!=0)
+		{
+			
+
+			$insert_query = $conn->prepare("INSERT INTO transactions (reg_no,receiver_reg_no,amount,description) VALUES (?,?,?,?)");
+			$insert_query->bind_param("ssis",$reg_no,$reg_no2,$amount,$desc);
+			if($insert_query->execute())
+			{
+				$balance_amt = $balance - $amount;
+				$query3 = "update balance set balance_amt = ".$balance_amt." where reg_no = '".$reg_no."';";
+				$balance_amtx = $balancex + $amount;
+				$query4 = "update balance set balance_amt = ".$balance_amtx." where reg_no = '".$reg_no2."';";
+
+				$stmnt1 = $conn->prepare($query3);
+				$stmnt2 = $conn->prepare($query4);
+				$stmnt1->execute();
+				$stmnt2->execute();
+
+				$message = "Transfer Successfull";
+				$code = 1;
+			}
+		}
+		else
+		{
+			$message = "Receiver doesnt exist or hasnt signed up with us";
+			$code = 2;
+		}
+	}
+	else
+	{
+		$message = "Unable to transfer funds as balance is low";
+		$code = 3;
+	}
+
+	$data = array("message"=>$message,"code"=>$code);
+
+	return $response->withJson($data)
 						->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
